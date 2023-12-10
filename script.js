3c58106e4d4daa81a0b5342ec388ce1958081512
John Durward <johnnydurward@gmail.com>
2023-12-10 14:22:07 -0500

    added class

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = '0',
+        read = false
+    ) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
 
 Book.prototype.toggleRead = function() {
